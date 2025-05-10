46a60c112bd9d9a5dad02029c3a8785dce5cdaa6
vab-gamer <s.vaibhavi2012@gmail.com>
2025-05-10 16:29:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,52 @@
-const initialBoard = [
-  [2, 2, 1, 1, 1, 2, 2],
-  [2, 2, 1, 1, 1, 2, 2],
+const boardLayout = [
+  [0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0],
   [1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 0, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1],
-  [2, 2, 1, 1, 1, 2, 2],
-  [2, 2, 1, 1, 1, 2, 2],
+  [0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0],
 ];
 
-let boardState = JSON.parse(JSON.stringify(initialBoard));
-let selected = null;
+let board = [];
+const gameBoard = document.getElementById("gameBoard");
+const resetBtn = document.getElementById("resetBtn");
 
-function renderBoard() {
-  const board = document.getElementById("board");
-  board.innerHTML = "";
-  for (let row = 0; row < boardState.length; row++) {
-    for (let col = 0; col < boardState[row].length; col++) {
+function createBoard() {
+  gameBoard.innerHTML = "";
+  board = JSON.parse(JSON.stringify(boardLayout));
+
+  for (let row = 0; row < 7; row++) {
+    for (let col = 0; col < 7; col++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
 
-      if (boardState[row][col] === 1) {
-        cell.classList.add("peg");
-      } else if (boardState[row][col] === 0) {
-        cell.classList.add("hole");
-      } else {
-        cell.style.visibility = "hidden";
+      if (board[row][col] === 1) {
+        const peg = document.createElement("div");
+        peg.classList.add("peg");
+        peg.dataset.row = row;
+        peg.dataset.col = col;
+        peg.onclick = () => handlePegClick(row, col);
+        cell.appendChild(peg);
+      } else if (board[row][col] === 0 && isValidCell(row, col)) {
+        const hole = document.createElement("div");
+        hole.classList.add("hole");
+        cell.appendChild(hole);
       }
 
-      cell.dataset.row = row;
-      cell.dataset.col = col;
-      cell.onclick = () => handleClick(row, col);
-      board.appendChild(cell);
+      gameBoard.appendChild(cell);
     }
   }
 }
 
-function handleClick(row, col) {
-  if (selected) {
-    const [sr, sc] = selected;
-    if (isValidMove(sr, sc, row, col)) {
-      makeMove(sr, sc, row, col);
-    }
-    selected = null;
-  } else if (boardState[row][col] === 1) {
-    selected = [row, col];
-  }
-  renderBoard();
+function isValidCell(row, col) {
+  return boardLayout[row][col] === 1;
 }
 
-function isValidMove(sr, sc, er, ec) {
-  const dr = er - sr;
-  const dc = ec - sc;
-
-  if (Math.abs(dr) === 2 && dc === 0) {
-    const mr = sr + dr / 2;
-    return boardState[er][ec] === 0 && boardState[mr][sc] === 1;
-  }
-
-  if (Math.abs(dc) === 2 && dr === 0) {
-    const mc = sc + dc / 2;
-    return boardState[er][ec] === 0 && boardState[sr][mc] === 1;
-  }
-
-  return false;
+function handlePegClick(row, col) {
+  // Placeholder logic for click
+  console.log(`Peg at ${row}, ${col} clicked`);
 }
 
-function makeMove(sr, sc, er, ec) {
-  boardState[er][ec] = 1;
-  boardState[sr][sc] = 0;
-  boardState[(sr + er) / 2][(sc + ec) / 2] = 0;
-}
-
-function resetGame() {
-  boardState = JSON.parse(JSON.stringify(initialBoard));
-  selected = null;
-  renderBoard();
-}
-
-renderBoard();
+resetBtn.onclick = createBoard;
+createBoard();
